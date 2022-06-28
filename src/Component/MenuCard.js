76c5f844a77126f2785ee93d3c9ad55c47f3ff2a
Harsh <harsharya7002@gmail.com>
2022-06-29 01:22:34 +0530

    using map function for access data from api

--- src/Component/MenuCard.js
+++ src/Component/MenuCard.js
@@ -9,19 +9,18 @@ const MenuCard = ({menuData}) => {
        menuData.map((curElem) => {
            return (
                <>
-            <div className='card-container'>
+            <div className='card-container' key={curElem.id}>
             <div className='card'>
                 <div className='card-body'>
-                    <span className='card-number card-circle subtle'>1</span>
-                    <span className='card-author subtle'>Breakfast</span> 
-                    <h2 className='card-title'>Maggi</h2>
+                    <span className='card-number card-circle subtle'>{curElem.id}</span>
+                    <span className='card-author subtle'>{curElem.name}</span> 
+                    <h2 className='card-title'>{curElem.name}</h2>
                     <span className='card-description subtle'>
-                        I Love maggie.akgakga agjalgnjalag magma.
-                        agakgma;ga.aglamblab
+                    {curElem.description}
                     </span>
                     <div className='card-read'>Read</div>
                 </div>
-                 {/* <img src = {image} alt = "images" className='card-media' />  */}
+                 <img src = {curElem.image} alt = "images" className='card-media' /> 
     
                 <span className='card-tag subtle'>Order Now</span> 
             </div>
